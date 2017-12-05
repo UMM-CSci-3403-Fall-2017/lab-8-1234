d610ba6645872e835aa756a0be2007377d57930f
Sungjae Park <park1637@neoprene.morris.umn.edu>
2017-12-04 20:57:59 -0600

    change code but have problem at class two

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,136 +1,131 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
+	static int min = Integer.MAX_VALUE;
 
-	public int globalResult = Integer.MAX_VALUE;
+	
+	@Override
+	public int minimumPairwiseDistance(int[] values) {
 
-	public synchronized int updateGlobalResult(int last) {
-		if(globalResult > last) {
-			globalResult = last;
+		min = Integer.MAX_VALUE;
+
+		Thread one = new Thread(new one(min, values));
+	   	Thread two = new Thread(new two(min, values));
+	    	Thread three = new Thread(new three(min, values));
+	    	Thread four = new Thread(new four(min, values));
+		
+		//start thread
+		one.start();
+	    	two.start();
+	    	three.start();
+	    	four.start();
+		
+		try {
+			one.join();
+		    	two.join();
+		    	three.join();
+		    	four.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		return globalResult;
-	}
-    @Override
-    public int minimumPairwiseDistance(int[] values) {
-	    Thread one = new Thread(new one(globalResult, values));
-	    Thread two = new Thread(new two(globalResult, values));
-	    Thread three = new Thread(new three(globalResult, values));
-	    Thread four = new Thread(new four(globalResult, values));
 
-	    one.start();
-	    two.start();
-	    three.start();
-	    four.start();
+	    return min;
+    }
 
-	    try {
-		    one.join();
-		    two.join();
-		    three.join();
-		    four.join();
-
-	    } catch (InterruptedException e) {
-		    e.printStackTrace();
+    public void updateGlobalResult(int lastResult) {
+	    if(lastResult < min){
+		    min = lastResult;
 	    }
-	    return globalResult;
-
-	    class one implements Runnable {
-		    private int[] mininumVal;
-		    private int minimums;
-
-		    private one(int min, int[] value) {
-			    mininumVal = min;
-			    minimums = value;
-			    
-			   // this.mininumVal = mininumVal;
-			   //this.minimum = minimum;
-		    }
-		    
-
-		    public void run() {
-			    int result = Integer.MAX_VALUE;
-			    for (int i = 0; i < values. length/2; i++) {
-				    for (int j = 0; j < i; j++) {
-					    int diff = Math.abs(values[i] - values[j]);
-					    if (diff < result) {
-						    result = diff;
-					    }
+    }
+    
+    class one implements Runnable {
+	    private int lastMinimum;
+	    private int [] values;
+	    
+	    one(int mins, int[] value) {
+		    lastMinimum = mins;
+		    values = value;
+	    }
+	    
+	    public void run() { 
+		    int result = Integer.MAX_VALUE;
+		    for (int i = 0; i < values. length/2; i++) {
+			    for (int j = 0; j < i; j++) {
+				    int diff = Math.abs(values[i] - values[j]);
+				    if (diff < result) {
+					    result = diff;
 				    }
 			    }
-			    updateGlobalResult(result);
 		    }
+		    updateGlobalResult(result);
+	    }
+    }
+    class two implements Runnable {
+	    private int lastMinimum;
+	    private int [] values;
+
+	    two(int mins, int[] value) {
+		    lastMinimum = mins;
+		    values = value;
 	    }
 
-	    class two implements Runnable {
-		    private int[] mininumVal;
-		    private int minimums;
-		    
-		    private two(int min, int[] value) {
-			    mininumVal = min;
-			    minimums = value;
-		    }
-		    
-
-		    public void run() {
-			    int result = Integer.MAX_VALUE;
-			    for (int i = 0; i < values.length/2; i++) {
-				    for (int j = 0; j < i; j++) {
-					    int dif = Math.abs(values[i] - values[j]);
-					    if (diff < result) {
-						    result = diff;
-					    }
+	    public void run() {
+		    int result = Integer.MAX_VALUE;
+		    for (int i = values.length/2; i < values.length; ++i) {
+			    for (int j = 0; j < i-values.length/2; ++j) {
+				    int dif = Math.abs(values[i] - values[j]);
+				    if (diff < result) {
+					    result = diff;
 				    }
 			    }
-			    updateGlobalResult(result);
 		    }
+		    updateGlobalResult(result);
 	    }
-
-	    class three implements Runnable {
-		    private int[] mininumVal;
-		    private int minimums;
-		    
-		    private three(int min, int[] value) {
-			    mininumVal = min;
-			    minimums = value;
-		    }
-		    
-
-		    public void run() {
-			    int result = Integer.MAX_VALUE;
-			    for (int i = 0; i < values.length/2; i++) {
-				    for (int j = 0; j < i; j++) {
-					    int diff = Math.abs(values[i] - values[j]);
-					    if (diff < result) {
-						    result = diff;
-					    }
-				    }
-			    }
-			    updateGlobalResult(result);
-		    }
+    }
+    
+    class three implements Runnable {
+	    private int lastMinimum;
+	    private int [] values;
+	    
+	    three(int minis, int[] value) {
+		    lastMinimum = mins;
+		    values = value;
 	    }
-
-	    class four implements Runnable {
-		    private int[] mininumVal;
-		    private int minimums;
-
-		    private four(int min, int[] value) {
-			    mininumVal = min;
-			    minimums = value;
-		    
-		    }
-		    
-
-		    public void run() {
-			    int reuslt = Integer.MAX_VALUE;
-			    for (int i = 0; i < values.length/2; i++) {
-				    for (int j = 0; j < i; j++) {
-					    int diff = math.abs(values[i] - values[j]);
-					    if (diff < result) {
-						    result = diff;
-					    }
+	    
+	    public void run() {
+		    int result = Integer.MAX_VALUE;
+		    for (int i = values.length/2; i < values.length; ++i) {
+			    for (int j = values.length/2; j < i; ++j) {
+				    int diff = Math.abs(values[i] - values[j]);
+				    if (diff < result) {
+					    result = diff;
 				    }
 			    }
-			    updateGlobalResult(result);
 		    }
+		    updateGlobalResult(result);
+	    }
+    }
+    
+    class four implements Runnable {
+	    private int lastMinimum;
+	    private int [] values;
+	    
+	    four(int mins, int[] value) {
+		    lastMinimum = mins;
+		    values = value;
+	    }
+	    
+	    public void run() {
+		    int result = Integer.MAX_VALUE;
+		    for (int j = 0; j < values.length/2; j++) {
+			    for (int i = values.length/2; i < j+values.length/2; ++i) {
+				    int diff = Math.abs(values[i] - values[j]);
+				    if (diff < result) {
+					    result = diff;
+				    }
+			    }
+		    }
+		    updateGlobalResult(result);
 	    }
     }
 }
